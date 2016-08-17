78737c3ff804cac2d509e853b56b7cb04cdfc8eb
memorysaver <mfa1484@gmail.com>
2016-08-17 21:56:14 +0800

    Download Script via git
    |------------------------------------------------|
    # Explain why this change is being made
    Download via git, so we can version control directly via .dotfile folder.
    
    # Provide links to any relevant tickets, articles or other resources

--- install.sh
+++ install.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
 
-[[ -x `command -v wget` ]] && CMD="wget --no-check-certificate -O -"
-[[ -x `command -v curl` ]] >/dev/null 2>&1 && CMD="curl -#L"
+clear
+echo "============================================"
+echo "     Memorysaver dotfiles Installation"
+echo "============================================"
 
-if [ -z "$CMD" ]; then
-  echo "No curl or wget available. Aborting."
+if [ "$(uname -s)" == "Darwin" ]; then
+  # macOS
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/memorysaver/dotfiles/master/macOS/installer.sh)"
 else
-  clear
-  echo "============================================"
-  echo "     Memorysaver dotfiles Installation"
-  echo "============================================"
-  mkdir -p "$HOME/.dotfiles" && \
-  eval "$CMD https://github.com/memorysaver/dotfiles/tarball/master | tar -xzv -C ~/.dotfiles --strip-components=1 --exclude='{.gitignore}'"
-
-  if [ "$(uname -s)" == "Darwin" ]; then
-    # macOS
-    . "$HOME/.dotfiles/macOS/installer.sh"
-  else
-    echo "Sorry! other platforms are not implemented yet."
-  fi
+  echo "Sorry! other platforms are not implemented yet."
 fi
--- macOS/installer.sh
+++ macOS/installer.sh
@@ -1,29 +1,39 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront
+sudo -v
+
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Update Homebrew recipes
-brew update && echo "Homebrew updated" || echo "Homebrew update failed"
+if test ! $(which brew); then
+brew update && echo "Homebrew updated"
+fi
 
 # install cool stuff first
-brew install git && echo "git installed"
-brew install figlet && echo "figlet installed"
-brew install toilet && echo "toilet installed"
+brew install git && echo "Git installed"
+
+if test ! $(which figlet); then
+  brew install figlet
+fi
+if test ! $(which figlet); then
+  brew install toilet
+fi
+
 clear
 toilet -f future Memorysaver Installation --filter metal
 toilet -f future _______________________ --filter metal
 
+# My dotfiles
+if [ ! -d "$HOME/.dotfiles" ]; then
+  git clone git@github.com:memorysaver/dotfiles.git ~/.dotfiles
+fi
+
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
 brew bundle --file=$HOME/.dotfiles/macOS/Brewfile
-
-# install oh-my-zsh
-sh -c "$(nohup curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh &)" && echo "ZSH installed"
-
-# generate ssh-keygen
-if [ ! -f "$HOME/.ssh/id_rsa" ]; then
-  ssh-keygen -t rsa -C "mfa1484@gmail.com"
-fi
 
 # Solarized terminal theme
 if [ ! -d "$HOME/solarized" ]; then
@@ -35,9 +45,24 @@ toilet -f future mac configuration --filter gay
 toilet -f future ________________ --filter gay
 source $HOME/.dotfiles/macOS/.macOS
 
-# Symlinks
-ln -sfv "$HOME/.dotfiles/mackup/.mackup.cfg" ~
-ln -sfv "$HOME/.dotfiles/.hammerspoon" ~
-ln -sfv "$HOME/.dotfiles/git/.gitconfig" ~
-ln -sfv "$HOME/.dotfiles/git/.gitignore_global" ~
-ln -sfv "$HOME/.dotfiles/git/.gitmessage" ~
+if [ ! -d "$HOME/.dotfiles" ]; then
+  echo "No dotfiles available. Aborting."
+else
+  # generate ssh-keygen
+  if [ ! -f "$HOME/.ssh/id_rsa" ]; then
+    ssh-keygen -t rsa -C "mfa1484@gmail.com"
+  fi
+
+  # Symlinks
+  ln -sfv "$HOME/.dotfiles/mackup/.mackup.cfg" ~
+  ln -sfv "$HOME/.dotfiles/.hammerspoon" ~
+  ln -sfv "$HOME/.dotfiles/git/.gitconfig" ~
+  ln -sfv "$HOME/.dotfiles/git/.gitignore_global" ~
+  ln -sfv "$HOME/.dotfiles/git/.gitmessage" ~
+
+  # install oh-my-zsh
+  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(nohup curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh &)" && echo "ZSH installed"
+  fi
+
+fi
